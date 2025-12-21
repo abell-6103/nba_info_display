f8dcafea03478a10a93cfec8a9e4de466c89425c
abell-6103 <andrewbell4829@gmail.com>
2025-12-20 18:11:30 -0800

    added screen for games display

--- frontend/app/(tabs)/_layout.tsx
+++ frontend/app/(tabs)/_layout.tsx
@@ -34,6 +34,15 @@ export default function TabLayout() {
           ),
         }}
       />
+      <Tabs.Screen
+        name="games"
+        options={{
+          title: 'Games',
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons name={focused ? 'information-circle' : 'information-circle-outline'} color={color} size={24}/>
+          ),
+        }}
+      />
     </Tabs>
   );
 }
---
+++ frontend/app/(tabs)/games.tsx
@@ -0,0 +1,10 @@
+import { View, Text} from 'react-native';
+import { styles } from '../styles';
+
+export default function Games() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Games</Text>
+    </View>
+  );
+}
