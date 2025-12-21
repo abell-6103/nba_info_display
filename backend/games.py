c952caac28456f4e6070b1f40c290a73756efcc4
abell-6103 <andrewbell4829@gmail.com>
2025-12-20 17:28:36 -0800

    getGamesFromDay() passes unit tests

--- backend/games.py
+++ backend/games.py
@@ -1,5 +1,6 @@
 from nba_api.stats.endpoints import scoreboardv3
 import pandas as pd
+import numpy as np
 from time import monotonic, sleep
 from datetime import date, timedelta
 
@@ -12,8 +13,56 @@ class GameInterface(ABC):
   def getGamesFromDay(self, day: date) -> dict:
     pass
 
-  def getGamesFromRange(self, start_day: date, end_day: date) -> dict:
+  def getGamesFromRange(self, start_day: date, end_day: date) -> list:
     pass
+
+class TeamObj:
+  def __init__(self):
+    self.id: int = None
+    self.city: str = None
+    self.score: int = None
+    self.logo: str = None
+
+  def toDict(self):
+    return {
+      "team_id" : self.id,
+      "city" : self.city,
+      "score" : self.score,
+      "logo" : self.logo
+    }
+
+class GameObj:
+  def __init__(self):
+    self.game_id: int = None
+    self.clock: str = None
+    self.period: str = None
+    self.status: int = None
+    self.status_text: str = None
+    self.home_team = TeamObj()
+    self.away_team = TeamObj()
+
+  def getPeriodStr(self):
+    if (self.period <= 4):
+      # Regulation
+      return f'Q{self.period}'
+    if (self.period == 5):
+      # Initial Overtime
+      return 'OT'
+    # Extra Overtime
+    return f'{self.period - 4}OT'
+
+  def getStatusStr(self):
+    if self.status != 2:
+      return self.status_text.strip()
+    return f"{self.getPeriodStr()} {self.clock}"
+
+  def toDict(self):
+    return {
+      "game_id" : int(self.game_id),
+      "status" : self.getStatusStr(),
+      "home_team" : self.home_team.toDict(),
+      "away_team" : self.away_team.toDict()
+    }
 
 class Games(GameInterface):
   def __init__(self, call_queue = CallQueue):
@@ -24,7 +73,22 @@ class Games(GameInterface):
 
     self.wait_time = 30
 
-  def getGamesFromDay(self, day: date) -> dict:
+  @staticmethod
+  def _buildTeamFromRow(row: pd.DataFrame) -> TeamObj:
+    row = row.iloc[0]
+    team = TeamObj()
+    
+    team.id = int(row['teamId'])
+    team.city = row['teamCity']
+    team.score = int(row['score'])
+    team.logo = f"https://cdn.nba.com/logos/nba/{team.id}/primary/L/logo.svg"
+
+    return team
+
+  def getGamesFromDay(self, day: date) -> list:
+    if not isinstance(day, date):
+      raise TypeError("day must be of type date")
+
     # If already cached and its been a short time, just return it from cache
     if day in self.games.keys() and (monotonic() - self.last_access[day]) <= self.wait_time:
       return self.games[day]
@@ -35,14 +99,36 @@ class Games(GameInterface):
     if (delay > 0):
       sleep(delay)
 
+    day_str = day.strftime('%m/%d/%Y')
+
     try:
-      day_str = day.strftime('%m/%d/%Y')
       found_games = scoreboardv3.ScoreboardV3(game_date=day_str)
     except:
       return None
+    self.last_access[day] = monotonic()
 
-    headers = found_games.game_header.get_data_frame()
-    linescores = found_games.line_score.get_data_frame()
+    headers = found_games.game_header.get_data_frame()[['gameId', 'gameStatus', 'gameStatusText', 'period', 'gameClock', 'gameCode']]
+    linescores = found_games.line_score.get_data_frame()[['gameId', 'teamId', 'teamCity', 'teamTricode', 'score']]
 
-    
-print(Games(CallQueue(4)).getGamesFromDay(date(2025,12,20)))
+    game_list = []
+    for _, row in headers.iterrows():
+      game = GameObj()
+
+      game.game_id = row.loc['gameId']
+      game.status = row.loc['gameStatus']
+      game.status_text = row.loc['gameStatusText']
+      game.period = row.loc['period']
+      game.clock = row.loc['gameClock']
+
+      team_codes = row.loc['gameCode'].split('/')[1]
+      hometeam_code = team_codes[0:3]
+      awayteam_code = team_codes[3:]
+      game_linescores = linescores.loc[linescores['gameId'] == game.game_id]
+
+      game.home_team = Games._buildTeamFromRow(game_linescores.loc[game_linescores['teamTricode'] == hometeam_code])
+      game.away_team = Games._buildTeamFromRow(game_linescores.loc[game_linescores['teamTricode'] == awayteam_code])
+
+      game_list.append(game.toDict())
+
+    self.games[day] = game_list
+    return game_list
--- backend/unit_games.py
+++ backend/unit_games.py
@@ -1,24 +1,85 @@
 import unittest
+
 from games import Games
+from callQueue import CallQueue
+
 from datetime import date
+import requests
 
 class TestGames(unittest.TestCase):
   def setUp(self):
-    self.games = Games()
+    self.delay = 2
+    self.call_queue = CallQueue(self.delay)
+    self.games = Games(self.call_queue)
+    self.valid_date = date(2025,12,20)
 
   def test_gamesfromday_badtype(self):
     badinput = "tuesday"
     with self.assertRaises(TypeError):
       self.games.getGamesFromDay(badinput)
 
-  def test_gamesfromday_badvalue(self):
-    badvalue = date(1902, 4, 20)
-    self.assertIsNone(self.games.getGamesFromDay(badvalue))
-
   def test_gamesfromday_noinput(self):
     with self.assertRaises(TypeError):
       self.games.getGamesFromDay(None)
 
+  def test_gamesfromday_nogames(self):
+    pre_nba_date = date(1900,1,1)
+    self.assertEqual(len(self.games.getGamesFromDay(pre_nba_date)), 0)
+
+  def test_gamesfromday_somegames(self):
+    self.assertGreater(len(self.games.getGamesFromDay(self.valid_date)), 0)
+
+  def test_gamesfromday_attributesnotnone(self):
+    res = self.games.getGamesFromDay(self.valid_date)
+    self.assertIsNotNone(res[0]['game_id'])
+    self.assertIsNotNone(res[0]['status'])
+    self.assertIsNotNone(res[0]['home_team'])
+    self.assertIsNotNone(res[0]['away_team'])
+
+  def test_gamesfromday_teamattributesnotnone(self):
+    res = self.games.getGamesFromDay(self.valid_date)
+    home_team = res[0]['home_team']
+    away_team = res[0]['away_team']
+
+    self.assertIsNotNone(home_team['team_id'])
+    self.assertIsNotNone(home_team['city'])
+    self.assertIsNotNone(home_team['score'])
+    self.assertIsNotNone(home_team['logo'])
+
+    self.assertIsNotNone(away_team['team_id'])
+    self.assertIsNotNone(away_team['city'])
+    self.assertIsNotNone(away_team['score'])
+    self.assertIsNotNone(away_team['logo'])
+
+  def test_gamesfromday_attributetypes(self):
+    res = self.games.getGamesFromDay(self.valid_date)
+    self.assertTrue(isinstance(res[0]['game_id'], int))
+    self.assertTrue(isinstance(res[0]['status'], str))
+    self.assertTrue(isinstance(res[0]['home_team'], dict))
+    self.assertTrue(isinstance(res[0]['away_team'], dict))
+
+  def test_gamesfromday_teamattributetypes(self):
+    res = self.games.getGamesFromDay(self.valid_date)
+    home_team = res[0]['home_team']
+    away_team = res[0]['away_team']
+
+    self.assertTrue(isinstance(home_team['team_id'], int))
+    self.assertTrue(isinstance(home_team['city'], str))
+    self.assertTrue(isinstance(home_team['score'], int))
+    self.assertTrue(isinstance(home_team['logo'], str))
+
+    self.assertTrue(isinstance(away_team['team_id'], int))
+    self.assertTrue(isinstance(away_team['city'], str))
+    self.assertTrue(isinstance(away_team['score'], int))
+    self.assertTrue(isinstance(away_team['logo'], str))
+
+  def test_gamesfromday_validlogos(self):
+    res = self.games.getGamesFromDay(self.valid_date)
+    home_team = res[0]['home_team']
+    away_team = res[0]['away_team']
+
+    self.assertEquals(requests.get(home_team['logo']).status_code, 200)
+    self.assertEquals(requests.get(away_team['logo']).status_code, 200)
 
 if __name__ == '__main__':
     unittest.main()
