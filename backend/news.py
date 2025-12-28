cee5d90af33ab59d94fe19596ed0fe12a7853035
abell-6103 <andrewbell4829@gmail.com>
2025-12-28 14:29:32 -0800

    wrote unit tests for news module, started writing news module (fetches nba.com and espn)

--- backend/news.py
+++ backend/news.py
@@ -1,7 +1,108 @@
 from pydantic import BaseModel
+from abc import ABC, abstractmethod
+
+import requests
+from bs4 import BeautifulSoup
+import json
+
+from time import monotonic, sleep
+
+ESPN_API_URL = "https://site.api.espn.com/apis/site/v2/sports/basketball/nba/news"
+NBA_NEWS_URL = "https://www.nba.com/news/category/top-stories"
 
 class ArticleInfo(BaseModel):
   title: str
   source: str
   href: str
   publish_time: str
+
+  def __repr__(self):
+    source_str = f"Source: {self.source}"
+    title_str = f"Title: {self.title}"
+    link_str = f"Link: {self.href}"
+    published_str = f"Published: {self.publish_time}"
+    return "\n".join([source_str, title_str, link_str, published_str])
+
+def _fetchEspnNews() -> list[ArticleInfo]:
+  # Request data
+  r = requests.get(ESPN_API_URL)
+  if not r.status_code == 200:
+    print("Unsuccessful request")
+    return []
+  
+  # Parse json
+  try:
+    response = r.json()
+  except requests.exceptions.JSONDecodeError:
+    return []
+  
+  # Generate and return list
+  res = []
+  espn_articles = response["articles"]
+  for article in espn_articles:
+    article_title = article['headline']
+    article_source = "ESPN"
+    article_href = article['links']['web']['href']
+    article_publish_time = article['published']
+    res.append(ArticleInfo(title=article_title, source=article_source, href=article_href,
+                           publish_time=article_publish_time))
+  return res
+
+def _fetchNbaDotComNews() -> list[ArticleInfo]:
+  # Request data
+  r = requests.get(NBA_NEWS_URL)
+  if not r.status_code == 200:
+    print("Unsuccessful request")
+    return []
+  
+  # Scrape for json
+  page_text = r.text
+  soup = BeautifulSoup(page_text, 'html.parser')
+  scripts = soup.find_all('script', attrs={"id": "__NEXT_DATA__"})
+  if len(scripts) <= 0:
+    print("No scripts found")
+    return []
+  response = json.loads(scripts[0].text)
+  try:
+    nba_articles = response['props']['pageProps']['category']['latest']['items']
+  except KeyError:
+    print("Couldn't find attributes")
+    return []
+
+  # Generate and return list
+  res = []
+  for article in nba_articles:
+    article_title = article['shortTitle']
+    article_source = "NBA.com"
+    article_href = article['permalink']
+    article_publish_time = article['date']
+    res.append(ArticleInfo(title=article_title, source=article_source, href=article_href,
+                           publish_time=article_publish_time))
+  return res
+  
+class NewsInterface(ABC):
+  @abstractmethod
+  def getNews(self) -> list[ArticleInfo]:
+    ...
+
+def _sortFunc(item: ArticleInfo) -> str:
+  return item.publish_time
+
+class News(NewsInterface):
+  def __init__(self):
+    self.cache = []
+    self.last_update = 0
+    self.wait_time = 60
+
+  def getNews(self) -> list[ArticleInfo]:
+    if (monotonic() - self.last_update) <= self.wait_time:
+      return self.cache
+    
+    espn_news = _fetchEspnNews()
+    nba_news = _fetchNbaDotComNews()
+
+    res = espn_news + nba_news
+    res.sort(key=_sortFunc, reverse=True)
+
+    self.cache = res
+    return res
---
+++ backend/unit_news.py
@@ -0,0 +1,56 @@
+import unittest
+
+import requests
+from datetime import datetime
+from time import sleep
+
+from news import News, ArticleInfo
+
+def valid_href(href: str):
+  delay = 6
+  sleep(delay)
+  r = requests.head(href, headers={'User-agent': 'Mozilla/5.0'})
+  redirect_codes = [301, 302, 303, 307, 308]
+  if r.status_code == 301:
+    new_href = r.headers['Location']
+    return valid_href(new_href)
+  return r.status_code == 200 or r.status_code == 304
+
+def valid_publish_time(publish_time: str):
+  iso_format = "%Y-%m-%dT%H:%M:%SZ"
+  try:
+    dt_obj = datetime.strptime(publish_time, iso_format)
+    return isinstance(dt_obj, datetime)
+  except ValueError:
+    return False
+  
+class TestNews(unittest.TestCase):
+  def setUp(self):
+    self.news = News()
+
+  def test_getnews_goodtyping(self):
+    res = self.news.getNews()
+    self.assertTrue(isinstance(res, list))
+    for item in res:
+      self.assertTrue(isinstance(item, ArticleInfo))
+  
+  def test_getnews_goodattrtyping(self):
+    res = self.news.getNews()
+    for item in res:
+      self.assertTrue(isinstance(item.title, str))
+      self.assertTrue(isinstance(item.source, str))
+      self.assertTrue(isinstance(item.href, str))
+      self.assertTrue(isinstance(item.publish_time, str))
+
+  def test_getnews_validhrefs(self):
+    res = self.news.getNews()
+    for item in res:
+      self.assertTrue(valid_href(item.href))
+
+  def test_getnews_validpublishtimes(self):
+    res = self.news.getNews()
+    for item in res:
+      self.assertTrue(valid_publish_time(item.publish_time))
+
+if __name__ == "__main__":
+  unittest.main()
